7063793c5856d4b8212ec12f7329e8c5b94efd1a
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2022-04-12 15:42:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,223 @@
+<html lang="fr">
+
+<head>
+    <meta name="generator" content="Hugo 0.91.2">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="assets/css/main.css" media="screen">
+    <link rel="stylesheet" href="assets/css/mainmin.css">
+    <title>L'événement étudiant de la communication responsable | Reboot !</title>
+    <meta name="description" content="L&amp;#39;événement étudiant de la communication responsable">
+    <meta property="og:title" content="L'événement étudiant de la communication responsable | Reboot !">
+    <meta property="og:description" content="L&amp;#39;événement étudiant de la communication responsable">
+    <meta property="og:image" content="/assets/images/share.jpg">
+    <meta property="og:url" content="/">
+    <meta property="og:type" content="website">
+    <meta name="twitter:title" content="L'événement étudiant de la communication responsable | Reboot !">
+    <meta name="twitter:description" content="L&amp;#39;événement étudiant de la communication responsable">
+    <meta name="twitter:image" content="/assets/images/share.jpg">
+    <meta name="twitter:card" content="summary_large_image">
+    <link rel="icon" type="image/png" href="/assets/images/favicons/favicon.png">
+    <meta name="apple-mobile-web-app-title" content="Reboot !">
+    <meta name="application-name" content="Reboot !">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="alternate" type="application/rss+xml" href="/index.xml" title="Reboot !">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" defer=""></script>
+</head>
+
+<body class="page__home">
+    <ul class="nav-accessibility">
+        <li><a href="#main">Accéder au contenu principal</a></li>
+        <li><a href="#navigation">Accéder au menu</a></li>
+    </ul>
+    <header role="banner">
+        <nav class="navbar" role="navigation" aria-label="Menu principal">
+            <div class="container">
+                <a class="logo" href="/"><img src="/assets/images/logo.svg" alt="Reboot !" height="52" width="272"></a>
+                <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Ouvrir / Fermer le menu">
+                    <span>Menu</span>
+                    <span></span>
+                </button>
+                <div class="menu" id="navbarNavDropdown">
+                    <ul class="nav-level-1">
+                        <li class="has-children"><a href="/programme/" id="dropdown-programme" role="button" data-bs-toggle="dropdown" aria-expanded="false">Programme</a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown-programme">
+                                <ul class="nav-level-2">
+                                    <li><a href="/programme/hello-world/">Hello world</a></li>
+                                    <li><a href="/programme/hackathon/">Hackathon</a></li>
+                                    <li><a href="/programme/climax-vendredi-18-novembre/">Climax</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li><a href="/a-propos/">Reboot ?</a></li>
+                        <li><a href="/partenaires/">Partenaires</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main id="main">
+        <header class="hero">
+            <div class="container">
+                <div>
+                    <h1>
+                        <p>L'événement étudiant de la communication responsable<br></p>
+                    </h1>
+                </div>
+                <nav aria-label="Fil d’arianne">
+                    <ol class="breadcrumb" itemscope="" itemtype="https://schema.org/BreadcrumbList">
+                        <li class="breadcrumb-item active" aria-current="page" itemprop="itemListElement" itemscope="" itemtype="https://schema.org/ListItem">
+                            <span itemprop="item">
+                                <span itemprop="name">Accueil</span>
+                                <meta itemprop="position" content="1">
+                            </span>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </header>
+        <div class="container">
+            <section>
+                <p><b>Réservez les dates, du 14 au 18 novembre 2022 !</b></p>
+                <p>Lundi 14 novembre, une journée de conférences d'introduction sur la communication responsable.&nbsp;<span>Du mardi 15 au jeudi 17 novembre, 3 jours de hackathon dans toutes les écoles participantes.&nbsp;</span><span>Vendredi 18 novembre, une journée de tables rondes et de débats, et la remise des Prix des meilleurs Hacks Reboot.</span></p>
+                <p><br></p>
+            </section>
+            <section>
+                <div class="top">
+                    <h2>
+                        <a href="/programme/">
+                            Programme
+                        </a>
+                    </h2>
+                    <div>
+                        <p>"Reboot!" vous propose 2 jours de conférences de grande qualité, 1 hackathon et des ressources pédagogiques en …</p>
+                    </div>
+                </div>
+                <div class="pages">
+                    <div>
+                        <div class="page">
+                            <div>
+                                <p class="title">
+                                    <a href="/programme/hello-world/" class="link">
+                                        Hello world
+                                    </a>
+                                </p>
+                                <p>Lundi 14 novembre, une journée de conférences d'introduction sur la communication responsable
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="page">
+                            <div>
+                                <p class="title">
+                                    <a href="/programme/hackathon/" class="link">
+                                        Hackathon
+                                    </a>
+                                </p>
+                                <p>Du 15 au 17 novembre, 3 jours de hackathon dans les écoles participantes soutenus par des personnalités et des …</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="page">
+                            <div>
+                                <p class="title">
+                                    <a href="/programme/climax-vendredi-18-novembre/" class="link">
+                                        Climax
+                                    </a>
+                                </p>
+                                <p>Vendredi 18 novembre, une journée de tables rondes et de débats, et la remise des Prix des meilleurs Hacks Reboot
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div class="top">
+                    <h2>
+                        <a href="/a-propos/">
+                            Reboot ?
+                        </a>
+                    </h2>
+                </div>
+            </section>
+            <section>
+                <div class="top">
+                    <h2>
+                        <a href="/partenaires/">
+                            Partenaires
+                        </a>
+                    </h2>
+                    <div>
+                        <p>Découvrez les partenaires de Reboot !
+                        </p>
+                    </div>
+                </div>
+            </section>
+        
+
+
+
+
+
+        <div class="figures">
+            <div class="figure"><span class="count">19</span>conférences</div>
+            <div class="figure"><span class="count">9</span>ateliers pratiques</div>
+            <div class="figure"><span class="count">8170</span>étudiantes et étudiants</div>
+            <div class="figure"><span class="count">32</span>heures de live sur Twitch</div>
+            <div class="figure"><span>1er</span>live français dans la catégorie Special Events</div>
+            <div class="figure"><span class="count">53</span>ressources pédagogiques</div>
+            <div class="figure"><span class="count">67</span>speakers</div>
+            <div class="figure"><span class="count">61</span>écoles de communication</div>
+            <div class="figure"><span class="count">47</span>bénévoles</div>
+            <div class="figure"><span class="count">51</span>partenaires</div>
+        </div>
+
+        </div>
+
+
+
+    </main>
+    <footer role="contentinfo">
+        <div class="container">
+            <div class="content">
+                <div>
+                    <a class="logo" href="/"><img src="/assets/images/reboot-button.svg" alt="Reboot !" height="174" width="339" loading="lazy"></a>
+                </div>
+                <div>
+                </div>
+                <div>
+                    <nav role="navigation" aria-label="Menu principal">
+                        <ul class="nav-level-1">
+                            <li class="has-children"><a href="/programme/">Programme</a></li>
+                            <li><a href="/a-propos/">Reboot ?</a></li>
+                            <li><a href="/partenaires/">Partenaires</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div>
+                <nav role="navigation" aria-label="Menu pages légales">
+                    <ul class="nav-legal nav-level-1">
+                    </ul>
+                </nav>
+                <nav aria-label="Menu réseaux sociaux">
+                    <ul class="nav-social nav-level-1">
+                        <li><a href="https://www.facebook.com/rebootcommunicationorg/" target="_blank" rel="noreferrer">Facebook</a></li>
+                        <li><a href="https://instagram.com/rebootcommunication/" target="_blank" rel="noreferrer">Instagram</a></li>
+                        <li><a href="https://twitter.com/Reboot_com/" target="_blank" rel="noreferrer">Twitter</a></li>
+                        <li><a href="https://www.linkedin.com/company/rebootcommunication/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+                        <li><a href="https://www.reboot2021.org/" target="_blank" rel="noreferrer">Reboot 2021</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </footer>
+    <script src="/js/main.js" defer=""></script>
+
+</body>
+
+</html>
